03ef0683ec8fbf3fbe73a738e2eb1870f524aa1c
mathieu-anderson <mathieu.hj.anderson@gmail.com>
2017-05-02 18:21:52 +0200

    Add nav component

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,45 @@
 <template>
   <div id="app">
-    <h1 @click="resetApp">tasteFight</h1>
+    <AppNav></AppNav>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  methods: {
-    resetApp: function () {
-      this.$store.commit('setMovieList', [])
-      this.$store.commit('setMovieName', '')
-      this.$store.commit('setMovieData', {})
-      this.$store.commit('setMyMovieRating', null)
-      this.$router.push('/')
+  import AppNav from '@/components/appNav'
+
+  export default {
+    name: 'app',
+    components: {
+      AppNav
+    },
+    methods: {
+      resetApp: function () {
+        this.$store.commit('setMovieList', [])
+        this.$store.commit('setMovieName', '')
+        this.$store.commit('setMovieData', {})
+        this.$store.commit('setMyMovieRating', null)
+        this.$router.push('/')
+      }
     }
   }
-}
 </script>
 
 <style>
-.pointer {
-  cursor: pointer;
-}
-h1 {
-  font-size: 4em;
-  font-weight: bold;
-  text-shadow: 1px 1px 4px rgba(150, 150, 150, 0.98);
-  cursor: pointer;
-}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: -2.5em;
+}
+body {
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/appNav.vue
@@ -0,0 +1,35 @@
+<template lang="html">
+  <nav>
+   <h1 @click="resetApp">tasteFight</h1>
+  </nav>
+</template>
+
+<script>
+export default {
+  methods: {
+    resetApp: function () {
+      this.$store.commit('setMovieList', [])
+      this.$store.commit('setMovieName', '')
+      this.$store.commit('setMovieData', {})
+      this.$store.commit('setMyMovieRating', null)
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style lang="css">
+nav {
+  background-color: #e9e9e9;
+  width: 100vw;
+  position: fixed;
+}
+h1 {
+  font-weight: bold;
+  text-shadow: 1px 1px 4px rgba(150, 150, 150, 0.98);
+  cursor: pointer;
+}
+.pointer {
+  cursor: pointer;
+}
+</style>
