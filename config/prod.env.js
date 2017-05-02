89b0d7da207070519f2261fb9a8aaa67d14d7bc0
mathieu-anderson <mathieu.hj.anderson@gmail.com>
2017-05-02 18:26:16 +0200

    Obfuscate API key

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
-  API_KEY: '"3afb334973093028cc5d28d0464b6383"'
+  // API_KEY: '"3afb334973093028cc5d28d0464b6383"'
+  API_KEY: process.env.API_KEY
 }
