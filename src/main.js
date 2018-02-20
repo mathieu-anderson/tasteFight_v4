cb2ce174cb387dc2df4b6216a5dcb65ee7af4e4e
Mathieu <mathieu@woorank.com>
2018-02-20 11:58:22 +0100

    Actually add GA

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueAnalytics from 'vue-analytics'
 import App from './App'
 import router from './router'
 import store from './store'
 
+Vue.use(VueAnalytics, {
+  id: 'UA-114397906-1'
+})
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
